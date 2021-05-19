b174c6afaa2ae949387514329677dfc4850dd2fb
crackprob <76407171+crackprob@users.noreply.github.com>
2021-05-19 19:47:45 +0000

    Deploying to gh-pages from  @ e5811b955de83e3d793d5380900b0c0463175875 🚀

---
+++ _next/static/05OK14p2g-ksyWa9YFZHM/_buildManifest.js
@@ -0,0 +1,1 @@
+self.__BUILD_MANIFEST={__rewrites:{beforeFiles:[],afterFiles:[],fallback:[]},"/":["static/css/57f8837ebd478251c738.css","static/chunks/pages/index-aa7c02e1f349e57c5f3b.js"],"/_error":["static/css/d68c25e2b42d9ec02de5.css","static/chunks/pages/_error-d9f09695ea963844e255.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
