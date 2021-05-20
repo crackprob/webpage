6cf0d3f99277342532a11cca61cf98ec8e7dd489
shubhamjagdhane <shubhamjagdhane1010@gmail.com>
2021-05-20 12:35:57 +0530

    made changes for responsive look

--- components/Layout.js
+++ components/Layout.js
@@ -3,14 +3,14 @@ import styles from "../styles/Layout.module.css";
 
 function Layout({ children }) {
   return (
-    <div className={styles.container}>
+    <div className={`${styles.container} ${styles.flex}`}>
       <Head>
         <title>Crackprob</title>
         <meta name="description" content="Crackprob's official page" />
       </Head>
-      <main className={styles.main}>
-        <h1>Welcome to Crackprob</h1>
-        <h4>Our website is still under construction...</h4>
+      <main className={`${styles.main} ${styles.flex}`}>
+        <h1>Welcome to crackprob</h1>
+        <h4>Our website is still under construction</h4>
       </main>
     </div>
   );
