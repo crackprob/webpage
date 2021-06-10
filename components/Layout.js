b59742259539ec3f82892263ec91d8bd7ea2e93e
crackprob <me.shubhamjagdhane@gmail.com>
2021-06-10 10:22:45 +0530

    removed the previous content

--- components/Layout.js
+++ components/Layout.js
@@ -13,7 +13,6 @@ function Layout({ children }) {
           Welcome to <span>crackprob</span>
         </h1>
         <h4>Our website is still under construction</h4>
-        <h3>Please visit after some time</h3>
       </main>
     </div>
   );
